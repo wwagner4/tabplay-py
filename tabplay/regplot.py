aa750848d6472d4b65ddfed04e3d4d04a0deaf83
wwagner4 <wwagner4@gmail.com>
2021-01-18 15:44:18 +0100

    some refactoring

--- tabplay/regplot.py
+++ tabplay/regplot.py
@@ -16,63 +16,55 @@ class RegConf:
     nam_y: str
 
 
-"""
-Index(['id', 'cont1', 'cont2', 'cont3', 'cont4', 'cont5', 'cont6', 'cont7',
-       'cont8', 'cont9', 'cont10', 'cont11', 'cont12', 'cont13', 'cont14',
-       'target'],
-"""
 files = Files()
-plot_id = '07'
-n = 50
-cols = 4
-var_ids = range(1, 14)
-
-reg_confs = [RegConf(nam_x=f'cont{i}', nam_y='target') for i in var_ids]
 
 
-def fs(v: float) -> int:
-    a = int(math.floor(v))
-    if a > (n - 1):
-        return n - 1
-    else:
-        return a
+def contour_all():
+    plot_id = '08'
+    n = 50
+    cols = 4
+    var_ids = range(1, 14)
+    reg_confs = [RegConf(nam_x=f'cont{i}', nam_y='target') for i in var_ids]
 
+    def fs(v: float) -> int:
+        a = int(math.floor(v))
+        if a > (n - 1):
+            return n - 1
+        else:
+            return a
 
-def rows(cnt: int) -> int:
-    return int(math.ceil(float(cnt) / cols))
+    def rows(cnt: int) -> int:
+        return int(math.ceil(float(cnt) / cols))
 
+    def plot_contour(reg_conf: RegConf, plot_cnt: int, plot_idx: int, df_scaled: pd.DataFrame):
+        values_x = df_scaled[reg_conf.nam_x].values
+        values_y = df_scaled[reg_conf.nam_y].values
 
-def plot1(reg_conf: RegConf, plot_cnt: int, plot_idx: int, df_scaled: pd.DataFrame):
-    values_x = df_scaled[reg_conf.nam_x].values
-    values_y = df_scaled[reg_conf.nam_y].values
+        value_pairs = zip(values_x, values_y)
 
-    value_pairs = zip(values_x, values_y)
+        erg = dict([(k, len(list(group))) for k, group in groupby(sorted(value_pairs))])
+        z = np.zeros((n, n), dtype=float)
+        for i in range(n):
+            for j in range(n):
+                z[i][j] = erg.get((i, j), 0)
 
-    erg = dict([(k, len(list(group))) for k, group in groupby(sorted(value_pairs))])
-    z = np.zeros((n, n), dtype=float)
-    for i in range(n):
-        for j in range(n):
-            z[i][j] = erg.get((i, j), 0)
+        max_cnt = np.amax(z)
+        z = np.sqrt(z / max_cnt)
+        x = np.arange(0, n).astype(float) / n
+        y = np.arange(0, n).astype(float) / n
 
-    max_cnt = np.amax(z)
-    z = np.sqrt(z / max_cnt)
-    x = np.arange(0, n).astype(float) / n
-    y = np.arange(0, n).astype(float) / n
+        rs = rows(plot_cnt)
+        print("subplot", rs, cols, plot_idx)
+        """
+        cmaps: ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+                'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
+        """
+        ax = plt.subplot(rs, cols, plot_idx)
+        # ax.contourf(x, y, z, cmap='Spectral', antialiased=True, levels=30)
+        ax.contour(x, y, z, cmap='PuOr', antialiased=True, levels=20)
+        ax.set_xlabel(reg_conf.nam_x)
+        ax.set_ylabel(reg_conf.nam_y)
 
-    rs = rows(plot_cnt)
-    print("subplot", rs, cols, plot_idx)
-    """
-    cmaps: ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
-            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
-    """
-    ax = plt.subplot(rs, cols, plot_idx)
-    # ax.contourf(x, y, z, cmap='Spectral', antialiased=True, levels=30)
-    ax.contour(x, y, z, cmap='Spectral', antialiased=True, levels=30)
-    ax.set_xlabel(reg_conf.nam_x)
-    ax.set_ylabel(reg_conf.nam_y)
-
-
-def main():
     scaler = MinMaxScaler(feature_range=(0, n))
     df = files.train_df()
     scaler.fit(df)
@@ -82,12 +74,29 @@ def main():
     cfg_cnt = len(reg_confs)
     plt.figure(figsize=(20, 17))
     for idx, reg_conf in zip(range(1, cfg_cnt + 1), reg_confs):
-        plot1(reg_conf, cfg_cnt, idx, df_scaled)
+        plot_contour(reg_conf, cfg_cnt, idx, df_scaled)
     plt.suptitle('predictors against target', fontsize=36)
     fnam = files.plotdir / f"regplot_{plot_id}.png"
     plt.savefig(fnam)
     print(f"Plotted to {fnam.absolute()}")
 
 
+def analyse_target():
+    def f(v: float) -> int:
+        if v < 4.0:
+            return 1
+        else:
+            return 0
+
+    df = files.train_df()
+    target = df.target.values
+    mi = min(target)
+    ma = max(target)
+    diff = ma - mi
+    print(mi, ma, diff)
+    fi = target < 3
+    print(fi)
+
+
 if __name__ == '__main__':
-    main()
+    analyse_target()
